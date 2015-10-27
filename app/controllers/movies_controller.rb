70917ed0dad2aa584521cf3deec168fc21b9bd09
Nick Jordan <cnicholasjordan@gmail.com>
2015-10-27 22:42:19 +0000

    display flash notices and alerts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,15 +16,15 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def create
-    @movie = Movie.new(movie_params)
+      def create
+        @movie = Movie.new(movie_params)
 
-    if @movie.save
-      redirect_to movies_path
-    else
-      render :new
-    end
-  end
+        if @movie.save
+          redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
+        else
+          render :new
+        end
+      end
 
   def update
     @movie = Movie.find(params[:id])
