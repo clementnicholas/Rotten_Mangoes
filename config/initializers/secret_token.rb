503fac15b8a4f8cfbe9dc46db99beb77ad95b5e5
Nick Jordan <cnicholasjordan@gmail.com>
2015-10-27 22:12:02 +0000

    first commit. new rails app, generated CRUD for movies model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangos::Application.config.secret_key_base = 'fb3dfda42be2fee2286100b6ffc308905614045273fc83797854c08be7eee544a1e8860fe9de06934a368f50d64df0b52bbea9cc02d035e61ed02d19bba92fb4'
