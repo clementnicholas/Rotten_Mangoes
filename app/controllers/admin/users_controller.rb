22806d42c137e700a7de54901df11dd562cf7101
Nick Jordan <cnicholasjordan@gmail.com>
2015-10-29 02:00:33 +0000

    admins can update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,11 +5,42 @@ class Admin::UsersController < AdminController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      render :new
+    end
   end
 
   def index
     @users = User.all.page(params[:page]).per(3)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
